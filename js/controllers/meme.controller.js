187d1d81e5c8d6622782ade3bb04057e1f097207
AIDcGH <aviv.c000@gmail.com>
2025-01-08 12:40:01 +0200

    Added basic structure

--- js/controllers/meme.controller.js
+++ js/controllers/meme.controller.js
@@ -1,2 +1,71 @@
 'use strict'
 
+function onInit() {
+
+}
+
+function renderMeme() {
+    
+}
+
+function onResize() {
+    
+}
+
+////////////////////// edit meme
+
+// canvas
+function onDown(ev) {
+    
+}
+
+function onMove(ev) {
+    
+}
+
+function onUp() {
+    
+}
+
+// text control
+function onBtnMove(amount) {
+    
+}
+
+function onSwitchLine() {
+    
+}
+
+function onAddLine(sticker = null) {
+    
+}
+
+function onDelLine() {
+    
+}
+
+// text editing
+function onFontSizeChange(amount) {
+    
+}
+
+function onChangeFont(font) {
+    
+}
+
+function onSetClr(isOutline = false) {
+    
+}
+
+// meme control
+function onSaveMeme() {
+    
+}
+
+function onDownloadMeme() {
+    
+}
+
+function onShareMeme() {
+    
+}
