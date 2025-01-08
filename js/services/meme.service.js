d5fa7ee7798c51f24ad95f39296444d263bae461
AIDcGH <aviv.c000@gmail.com>
2025-01-08 13:04:34 +0200

    Wednesday 14:00

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,2 +1,16 @@
 'use strict'
 
+const gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+
+var gMeme = {
+    selectedImgId,
+    selectedLineIdx,
+    lines: [
+        {
+            txt,
+            size,
+            color
+        }
+    ]
+}
+const gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
