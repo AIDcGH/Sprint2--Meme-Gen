5f09c9110d1a9dc0c8feab27b4044469f1957d7f
AIDcGH <aviv.c000@gmail.com>
2025-01-08 18:18:23 +0200

    Added text control to service

--- js/controllers/meme.controller.js
+++ js/controllers/meme.controller.js
@@ -12,7 +12,7 @@ function onResize() {
     
 }
 
-////////////////////// edit meme
+////////////////////// generate meme
 
 // canvas
 function onDown(ev) {
@@ -28,6 +28,10 @@ function onUp() {
 }
 
 // text control
+function onText(txt) {
+    
+}
+
 function onBtnMove(amount) {
     
 }
@@ -45,7 +49,7 @@ function onDelLine() {
 }
 
 // text editing
-function onFontSizeChange(amount) {
+function onChangeFontSize(amount) {
     
 }
 
