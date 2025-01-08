a33fb1b01d1c4124ae5cde0e8e19bba859cd1440
AIDcGH <aviv.c000@gmail.com>
2025-01-08 10:29:59 +0200

    Added some utils for later

--- js/services/utils.service.js
+++ js/services/utils.service.js
@@ -1,2 +1,27 @@
 'use strict'
 
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min)
+    const maxFloored = Math.floor(max)
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+}
+
+function makeId(length = 6) {
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+	var id = ''
+	for (var i = 0; i < length; i++) {
+		id += possible.charAt(getRandomInt(0, possible.length))
+	}
+	return id
+}
+
+//storage
+function saveToStorage(key, value) {
+    var json = JSON.stringify(value)
+    localStorage.setItem(key, json)
+}
+
+function loadFromStorage(key) {
+    var json = localStorage.getItem(key)
+    return JSON.parse(json)
+}
